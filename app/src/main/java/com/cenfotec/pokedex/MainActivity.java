b4a9df5e132c9471a849af24b572cd97b8a775b7
Mary Paz Araya <mary_zap@hotmail.com>
2020-02-13 10:44:48 -0600

    Google Sign In intent verification
    Verify is user is already signed in

--- app/src/main/java/com/cenfotec/pokedex/MainActivity.java
+++ app/src/main/java/com/cenfotec/pokedex/MainActivity.java
@@ -1,11 +1,14 @@
 package com.cenfotec.pokedex;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -14,9 +17,12 @@ import com.cenfotec.pokedex.models.Pokemon;
 import com.cenfotec.pokedex.models.PokemonResponse;
 import com.cenfotec.pokedex.service.PokeService;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 import java.util.ArrayList;
 
@@ -99,6 +105,48 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    //To manage the Google Sign-In intent
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == Activity.RESULT_OK)
+            switch (requestCode){
+                case 101:
+                    try {
+                        // The Task returned from this call is always completed, no need to attach a listener.
+
+                        Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+                        GoogleSignInAccount account = task.getResult(ApiException.class);
+                        onLoggedIn(account);
+                    }catch (ApiException e) {
+
+                        //The ApiException status code indicates the detailed failure reason
+                        Log.w(TAG, "signInResult: failed code " + e.getStatusCode() );
+                    }
+                    break;
+            }
+    }
+
+    //To verify if user is already logged into google account
+    @Override
+    protected void onStart() {
+        super.onStart();
+        GoogleSignInAccount alreadyloggedAccount = GoogleSignIn.getLastSignedInAccount(this);
+        if(alreadyloggedAccount != null){
+            Toast.makeText(this, "Already Logged In", Toast.LENGTH_SHORT).show();
+            onLoggedIn(alreadyloggedAccount);
+        }else{
+            Log.d(TAG, "onStart: Not logged in ");
+        }
+    }
+
+    private void onLoggedIn(GoogleSignInAccount googleSignInAccount){
+        Intent intent = new Intent(this, ProfileActivity.class);
+        intent.putExtra(ProfileActivity.GOOGLE_ACCOUNT, googleSignInAccount);
+
+        startActivity(intent);
+        finish();
+    }
 
     private void getData(int offset){
 
