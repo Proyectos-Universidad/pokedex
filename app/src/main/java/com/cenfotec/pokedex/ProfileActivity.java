9bf18af37a8fe2cf24069b3b7dd1e963767fefd2
Mary Paz Araya <mary_zap@hotmail.com>
2020-02-13 13:37:07 -0600

    Profile Activity to test log in

--- app/src/main/java/com/cenfotec/pokedex/ProfileActivity.java
+++ app/src/main/java/com/cenfotec/pokedex/ProfileActivity.java
@@ -1,6 +1,68 @@
 package com.cenfotec.pokedex;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.squareup.picasso.Picasso;
+
 public class ProfileActivity extends AppCompatActivity {
+
+    public static final String GOOGLE_ACCOUNT = "google_account";
+
+    private TextView profileName, profileEmail;
+    private ImageView profileImage;
+
+    private Button signOut;
+    private GoogleSignInClient googleSignInClient;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        profileName = findViewById(R.id.profile_text);
+        profileEmail = findViewById(R.id.profile_email);
+        profileImage = findViewById(R.id.profile_image);
+        signOut = findViewById(R.id.sign_out);
+
+        setDataOnView();
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        signOut.setOnClickListener( v -> {
+
+            googleSignInClient.signOut().addOnCompleteListener(task -> {
+                Intent intent = new Intent(ProfileActivity.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            });
+        });
+    }
+
+    private void setDataOnView() {
+
+        GoogleSignInAccount googleSignInAccount = getIntent().getParcelableExtra(GOOGLE_ACCOUNT);
+
+        Picasso.get().load(googleSignInAccount.getPhotoUrl()).centerInside().fit().into(profileImage);
+        profileName.setText(googleSignInAccount.getDisplayName());
+        profileEmail.setText(googleSignInAccount.getEmail());
+
+
+    }
+
+
 }
