33b6dc4acf00403ba5c76aace3a4736f79df5bcb
Paz <mary_zap@hotmail.com>
2020-02-13 18:06:10 -0600

    Updates to app

--- app/src/main/java/com/cenfotec/pokedex/MainActivity.java
+++ app/src/main/java/com/cenfotec/pokedex/MainActivity.java
@@ -6,15 +6,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import com.cenfotec.pokedex.models.Pokemon;
-import com.cenfotec.pokedex.models.PokemonResponse;
-import com.cenfotec.pokedex.service.PokeService;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -23,14 +17,6 @@ import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
 
-import java.util.ArrayList;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,13 +24,6 @@ public class MainActivity extends AppCompatActivity {
     public GoogleSignInClient googleSignInClient;
     private SignInButton googleSignInButton;
 
-    private Retrofit retrofit;
-
-    private RecyclerView recyclerView;
-    private ListPokemonAdapter listPokemonAdapter;
-
-    private int offset;
-    private boolean clearedToLoad;
 
     //To manage the Google Sign-In intent
     @Override
@@ -97,45 +76,7 @@ public class MainActivity extends AppCompatActivity {
             startActivityForResult(signInIntent, 101);
         });
 
-        recyclerView = findViewById(R.id.recyclerView);
-        listPokemonAdapter = new ListPokemonAdapter(this);
-        recyclerView.setAdapter(listPokemonAdapter);
-        recyclerView.setHasFixedSize(true);
-        final GridLayoutManager layoutManager = new GridLayoutManager(this, 3);
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
-            @Override
-            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
-                super.onScrolled(recyclerView, dx, dy);
-
-                if(dy > 0){
-                    int visibleItemCount = layoutManager.getChildCount();
-                    int totalItemCount = layoutManager.getItemCount();
-                    int pastVisibleItems = layoutManager.findFirstVisibleItemPosition();
-
-                    if(clearedToLoad){
-                        if((visibleItemCount + pastVisibleItems) >= totalItemCount){
-                            clearedToLoad = false;
-                            offset += 20;
-                            getData(offset);
-                        }
-                    }
-                }
-            }
-        });
-
-        retrofit = new Retrofit.Builder()
-                .baseUrl("https://pokeapi.co/api/v2/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-
-        clearedToLoad = true;
-        offset = 0;
-        getData(offset);
-
-
     }
-
 
 
     private void onLoggedIn(GoogleSignInAccount googleSignInAccount){
@@ -146,31 +87,4 @@ public class MainActivity extends AppCompatActivity {
         finish();
     }
 
-    private void getData(int offset){
-
-        PokeService service = retrofit.create(PokeService.class);
-        Call<PokemonResponse> pokemonResponseCall = service.getPokemonList(20, offset);
-
-        pokemonResponseCall.enqueue(new Callback<PokemonResponse>() {
-            @Override
-            public void onResponse(Call<PokemonResponse> call, Response<PokemonResponse> response) {
-                clearedToLoad = true;
-                if(response.isSuccessful()){
-
-                    PokemonResponse pokemonResponse = response.body();
-                    ArrayList<Pokemon> pokemonList = pokemonResponse.getResults();
-
-                    listPokemonAdapter.addPokemonList(pokemonList);
-                }else{
-                    Log.e(TAG, "onResponse: " + response.errorBody() );
-                }
-            }
-
-            @Override
-            public void onFailure(Call<PokemonResponse> call, Throwable t) {
-                clearedToLoad = true;
-                Log.e(TAG, "onFailure: " + t.getMessage());
-            }
-        });
-    }
 }
--- app/src/main/java/com/cenfotec/pokedex/ProfileActivity.java
+++ app/src/main/java/com/cenfotec/pokedex/ProfileActivity.java
@@ -1,23 +1,42 @@
 package com.cenfotec.pokedex;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.cenfotec.pokedex.models.Pokemon;
+import com.cenfotec.pokedex.models.PokemonResponse;
+import com.cenfotec.pokedex.service.PokeService;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ProfileActivity extends AppCompatActivity {
 
     public static final String GOOGLE_ACCOUNT = "google_account";
+    private static final String TAG = "ProfileActivity";
+
+    private Retrofit retrofit;
+
+    private RecyclerView recyclerView;
+    private ListPokemonAdapter listPokemonAdapter;
+
+    private int offset;
+    private boolean clearedToLoad;
 
     private TextView profileName, profileEmail;
     private ImageView profileImage;
@@ -30,39 +49,102 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        profileName = findViewById(R.id.profile_text);
-        profileEmail = findViewById(R.id.profile_email);
-        profileImage = findViewById(R.id.profile_image);
-        signOut = findViewById(R.id.sign_out);
+//        profileName = findViewById(R.id.profile_text);
+//        profileEmail = findViewById(R.id.profile_email);
+//        profileImage = findViewById(R.id.profile_image);
+//        signOut = findViewById(R.id.sign_out);
+//
+//        setDataOnView();
+//
+//        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+//                .requestEmail()
+//                .build();
+//
+//        googleSignInClient = GoogleSignIn.getClient(this, gso);
+//
+//        signOut.setOnClickListener( v -> {
+//
+//            googleSignInClient.signOut().addOnCompleteListener(task -> {
+//                Intent intent = new Intent(ProfileActivity.this, MainActivity.class);
+//                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//                startActivity(intent);
+//            });
+//        });
 
-        setDataOnView();
+        recyclerView = findViewById(R.id.recyclerView);
+        listPokemonAdapter = new ListPokemonAdapter(this);
+        recyclerView.setAdapter(listPokemonAdapter);
+        recyclerView.setHasFixedSize(true);
+        final GridLayoutManager layoutManager = new GridLayoutManager(this, 3);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
 
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail()
+                if(dy > 0){
+                    int visibleItemCount = layoutManager.getChildCount();
+                    int totalItemCount = layoutManager.getItemCount();
+                    int pastVisibleItems = layoutManager.findFirstVisibleItemPosition();
+
+                    if(clearedToLoad){
+                        if((visibleItemCount + pastVisibleItems) >= totalItemCount){
+                            clearedToLoad = false;
+                            offset += 20;
+                            getData(offset);
+                        }
+                    }
+                }
+            }
+        });
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl("https://pokeapi.co/api/v2/")
+                .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
-        googleSignInClient = GoogleSignIn.getClient(this, gso);
+        clearedToLoad = true;
+        offset = 0;
+        getData(offset);
+    }
 
-        signOut.setOnClickListener( v -> {
+//    private void setDataOnView() {
+//
+//        GoogleSignInAccount googleSignInAccount = getIntent().getParcelableExtra(GOOGLE_ACCOUNT);
+//
+//        Picasso.get().load(googleSignInAccount.getPhotoUrl()).centerInside().fit().into(profileImage);
+//        profileName.setText(googleSignInAccount.getDisplayName());
+//        profileEmail.setText(googleSignInAccount.getEmail());
+//
+//
+//    }
 
-            googleSignInClient.signOut().addOnCompleteListener(task -> {
-                Intent intent = new Intent(ProfileActivity.this, MainActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(intent);
-            });
+    private void getData(int offset){
+
+        PokeService service = retrofit.create(PokeService.class);
+        Call<PokemonResponse> pokemonResponseCall = service.getPokemonList(20, offset);
+
+        pokemonResponseCall.enqueue(new Callback<PokemonResponse>() {
+            @Override
+            public void onResponse(Call<PokemonResponse> call, Response<PokemonResponse> response) {
+                clearedToLoad = true;
+                if(response.isSuccessful()){
+
+                    PokemonResponse pokemonResponse = response.body();
+                    ArrayList<Pokemon> pokemonList = pokemonResponse.getResults();
+
+                    listPokemonAdapter.addPokemonList(pokemonList);
+                }else{
+                    Log.e(TAG, "onResponse: " + response.errorBody() );
+                }
+            }
+
+            @Override
+            public void onFailure(Call<PokemonResponse> call, Throwable t) {
+                clearedToLoad = true;
+                Log.e(TAG, "onFailure: " + t.getMessage());
+            }
         });
     }
-
-    private void setDataOnView() {
-
-        GoogleSignInAccount googleSignInAccount = getIntent().getParcelableExtra(GOOGLE_ACCOUNT);
-
-        Picasso.get().load(googleSignInAccount.getPhotoUrl()).centerInside().fit().into(profileImage);
-        profileName.setText(googleSignInAccount.getDisplayName());
-        profileEmail.setText(googleSignInAccount.getEmail());
-
-
-    }
-
 
 }
